03199e1438bab19ccd65bb2630548b0a3e4ea471
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2015-06-30 16:11:46 +0200

    wrapper functions for system calls

--- user/src/user.cc
+++ user/src/user.cc
@@ -1,3 +1,21 @@
+unsigned long syscall1(unsigned long w0)
+{
+    asm volatile("syscall" : "+D"(w0) : : "rcx", "r11", "memory");
+    return w0;
+}
+
+unsigned long syscall2(unsigned long w0, unsigned long w1)
+{
+    asm volatile("syscall" : "+D"(w0) : "S"(w1) : "rcx", "r11", "memory");
+    return w0;
+}
+
+// example syscall for yielding
+unsigned long sys_yield()
+{
+    return syscall1(0xdeadbeaf /* TODO use correct syscall number*/);
+}
+
 extern "C" [[noreturn]]
 void main_func()
 {
