c96aea029c4a4e3ee5f518c920abe7bdc4d61556
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2015-06-30 14:21:02 +0200

    user code to invoke both system calls

--- kern/src/usercode.cc
+++ kern/src/usercode.cc
@@ -1,10 +1,46 @@
-extern "C" [[gnu::regparm(1), gnu::section(".user"), noreturn]]
-void usercode(unsigned)
+#include "compiler.h"
+
+/*
+ * x86-64 SYSCALL protocol:
+ *   CPU saves: RIP -> RCX, RFLAGS -> R11 (then masks RFLAGS with IA32_FMASK)
+ *   CPU loads: CS/SS from STAR MSR, RIP from LSTAR MSR
+ *   After syscall returns (sysretq): RIP <- RCX, RFLAGS <- R11
+ *
+ * The kernel entry (entry_sysenter in entry.S) reads the syscall number from
+ * the low 8 bits of RDI and the arguments from RSI/RDI.
+ */
+
+[[gnu::section(".user")]]
+static unsigned long syscall1(unsigned long w0)
 {
-    // TODO
-    // - 1st : ud2 or force page fault here
-    // - 2nd : reenter the kernel via sysenter
-    // - 3rd : so some simple system calls, like adding two numbers
+    asm volatile("syscall" : "+D"(w0) : : "rcx", "r11", "memory");
+    return w0;
+}
+
+[[gnu::section(".user")]]
+static unsigned long syscall3(unsigned long w0, unsigned long w1, unsigned long w2)
+{
+    asm volatile("syscall" : "+D"(w0) : "S"(w1), "d"(w2) : "rcx", "r11", "memory");
+    return w0;
+}
+
+[[gnu::section(".user")]]
+static unsigned long sys_nop()
+{
+    return syscall1(0);
+}
+
+[[gnu::section(".user")]]
+static unsigned long sys_add(unsigned long a, unsigned long b)
+{
+    return syscall3(1, a, b);
+}
+
+extern "C" [[noreturn]] [[gnu::section(".user")]]
+void usercode()
+{
+    sys_nop();
+    sys_add(2, 3);
 
     while (1)
         ;
