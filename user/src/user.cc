c46dd119600f4c744108fc3b702f5c42c8489313
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2015-07-01 11:58:29 +0200

    create threads in userland and switch between them

--- user/src/user.cc
+++ user/src/user.cc
@@ -10,15 +10,39 @@ unsigned long syscall2(unsigned long w0, unsigned long w1)
     return w0;
 }
 
-// example syscall for yielding
+unsigned long syscall3(unsigned long w0, unsigned long w1, unsigned long w2)
+{
+    asm volatile("syscall" : "+D"(w0) : "S"(w1), "d"(w2) : "rcx", "r11", "memory");
+    return w0;
+}
+
+void sys_create_ec(void (*eip)(), void *esp)
+{
+    syscall3(1, reinterpret_cast<unsigned long>(eip), reinterpret_cast<unsigned long>(esp));
+}
+
 unsigned long sys_yield()
 {
-    return syscall1(0xdeadbeaf /* TODO use correct syscall number*/);
+    return syscall1(2);
+}
+
+[[noreturn]]
+void thread()
+{
+    while (1)
+        sys_yield();
 }
 
 extern "C" [[noreturn]]
 void main_func()
 {
+    char stack[512];
+
+    for (int i = 1; i <= 8; i++) {
+        sys_create_ec(thread, stack + i * 64);
+        sys_yield();
+    }
+
     while (1)
-        ;
+        sys_yield();
 }
